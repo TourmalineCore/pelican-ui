f8de794350970f31ba317ca49e04d99353652d4c
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2024-11-01 11:39:29 +0500

    chore: change playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig<WorkerConfigOptions>({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: `on-first-retry`,
     webServer: {
-      command: `npm run`,
+      command: `next`,
       args: [`dev`],
       url: `http://localhost:3000`,
     },
