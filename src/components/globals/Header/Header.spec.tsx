c44faa9b0b0ead112e2b009a95c533c0dfad3ace
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-07-28 15:03:32 +0500

    test: add header and hero contact link href attribute tests

--- src/components/globals/Header/Header.spec.tsx
+++ src/components/globals/Header/Header.spec.tsx
@@ -1,5 +1,5 @@
 import { BREAKPOINTS } from '@/playwright-tests/constants/breakpoints';
-import { test } from '@/playwright-tests/custom-test';
+import { expect, Page, test } from '@/playwright-tests/custom-test';
 import { Breakpoint, ComponentName } from '@/src/common/enum';
 
 test.describe(`HeaderComponentTests`, () => {
@@ -26,4 +26,26 @@ test.describe(`HeaderComponentTests`, () => {
       });
     });
   }
+
+  test(`HeaderContactLinkAttributeCheckTest`, HeaderContactLinkAttributeCheck);
 });
+
+async function HeaderContactLinkAttributeCheck({
+  page,
+}: {
+  page: Page;
+}) {
+  await page.setViewportSize({
+    width: 1366,
+    height: 768,
+  });
+
+  const contactLink = await page.locator(`a[href^="mailto:"]`)
+    .first();
+
+  const expectedEmail = await contactLink.getAttribute(`href`);
+
+  // TODO: Check why there is a build error when importing the MOCK_EMAIL constant
+  await expect(expectedEmail)
+    .toBe(`mailto:metodist@chelzoo.ru`);
+}
--- src/components/home-page/HomepageHero/HomepageHero.spec.tsx
+++ src/components/home-page/HomepageHero/HomepageHero.spec.tsx
@@ -1,5 +1,5 @@
 import { BREAKPOINTS } from '@/playwright-tests/constants/breakpoints';
-import { test } from '@/playwright-tests/custom-test';
+import { expect, Page, test } from '@/playwright-tests/custom-test';
 import { ComponentName } from '@/src/common/enum';
 
 test.describe(`HomepageHeroComponentTests`, () => {
@@ -24,4 +24,26 @@ test.describe(`HomepageHeroComponentTests`, () => {
       });
     });
   }
+
+  test(`HomepageheroContactLinkAttributeCheckTest`, HomepageheroContactLinkAttributeCheck);
 });
+
+async function HomepageheroContactLinkAttributeCheck({
+  page,
+}: {
+  page: Page;
+}) {
+  await page.setViewportSize({
+    width: 375,
+    height: 768,
+  });
+
+  const contactLink = await page.locator(`a[href^="tel:"]`)
+    .first();
+
+  const expectedPhone = await contactLink.getAttribute(`href`);
+
+  // TODO: Check why there is a build error when importing the MOCK_PHONE constant
+  await expect(expectedPhone)
+    .toBe(`tel:+7 (351) 263-18-64`);
+}
