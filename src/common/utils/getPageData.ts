765514ed1abab02f54adb95e89a5784479e224da
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-09-01 08:31:40 +0500

    fix: add query for steps block

--- src/common/utils/getPageData.ts
+++ src/common/utils/getPageData.ts
@@ -98,6 +98,7 @@ export async function getPageData({
           `blocks.scheduleCard`,
           `blocks.scheduleCard.timetable`,
           `blocks.image`,
+          `blocks.stepsCards`,
           `seo`,
         ],
         isOtherPage: true,
