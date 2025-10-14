22a428c94c6361d73999884792d20501c8406ca9
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-10-14 15:04:30 +0500

    fix(TextAndMedia): fix text wrap to prevent the text from entering the picture (#430)
    * fix(TextAndMedia): fix text wrap to prevent the text from entering the picture
    
    * feat: add test on text-and-media-with-long-title

--- src/components/globals/TextAndMedia/TextAndMedia.spec.tsx
+++ src/components/globals/TextAndMedia/TextAndMedia.spec.tsx
@@ -1,6 +1,6 @@
 import { BREAKPOINTS } from '@/playwright-tests/constants/breakpoints';
-import { test } from '@/playwright-tests/custom-test';
-import { ComponentName } from '@/src/common/enum';
+import { expect, test } from '@/playwright-tests/custom-test';
+import { Breakpoint, BreakpointName, ComponentName } from '@/src/common/enum';
 
 test.describe(`TextAndMediaComponentTests`, () => {
   test.beforeEach(async ({
@@ -24,4 +24,29 @@ test.describe(`TextAndMediaComponentTests`, () => {
       });
     });
   }
+});
+
+test.describe(`TextAndMediaWithLongTitleComponentTests`, () => {
+  test.beforeEach(async ({
+    goToComponentsPage,
+  }) => {
+    await goToComponentsPage(ComponentName.TEXT_AND_MEDIA_WITH_LONG_TITLE);
+  });
+
+  test(`DesktopXlTest`, async ({
+    page,
+    setViewportSize,
+  }) => {
+    // This is necessary so that the tests do not crop the screenshots.
+    await page.addStyleTag({
+      content: `html, body, #__next { height: auto !important; min-height: 100% !important; }`,
+    });
+
+    await setViewportSize({
+      width: Breakpoint.DESKTOP_XL,
+    });
+
+    await expect(page.getByTestId(`text-and-media`))
+      .toHaveScreenshot(`text-and-media-with-long-title-${BreakpointName.DESKTOP_XL}.png`);
+  });
 });
